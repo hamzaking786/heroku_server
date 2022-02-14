310ebd56a238dbec3f1db53489594bd7be8c2dcf
Hamza Dastgir <m.hamzadastgir@outlook.com>
2022-02-14 20:51:41 +0100

    Use port fron enviroment for Heorku fixed

--- server/server.js
+++ server/server.js
@@ -10,6 +10,6 @@ app.post("/login", (req, res, next)=> {
     res.sendStatus(401);
 })
 
-app.listen(process.env.PORT || 3000, ()=> {
+const server = app.listen( process.env.PORT || 3000, () => {
     console.log(`Started on http://localhost:${server.address().port}`);
 });
