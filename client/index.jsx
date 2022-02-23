50a6a607a473014bdf4487307b64b23f0ad7df66
Hamza Dastgir <m.hamzadastgir@outlook.com>
2022-02-23 18:24:02 +0100

    Tell Heroku how to build client fix now?hm

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <ul>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/api/login", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <div>
+        Username{" "}
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        Password{" "}
+        <input
+          type={"password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/*"} element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/login", (req, res, next) => {
-  res.json({ username: "Noen Andre Persson" });
+app.get("/api/login", (req, res, next) => {
+  res.json({ username: "Noen André Persson" });
 });
-
-app.post("/login", (req, res, next) => {
+app.post("/api/login", (req, res, next) => {
   res.sendStatus(401);
 });
 
 app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
